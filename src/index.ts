58eae41e1f68316c74943e68c1687f2f249dbf41
RocioLdlT <r.delatorre.f95@gmail.com>
2026-05-08 13:23:33 +0100

    Make our code in index smaller by separating the server and a handler that listens to it

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { env } from './config/env.ts';
+import debug from 'debug';
+import {startServer} from './server.ts'
+
+const log = debug(`${env.PROJECT_NAME}:index`);
+
+
+await startServer().catch((error) => {
+    log('Error starting server:', error);
+    process.exit(1);
+});
